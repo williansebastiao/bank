d0d74e3e1db62166500b287078e285a5aa643248
Willians Pereira <willians@4vconnect.com>
2022-02-18 18:39:58 -0300

    feature: verify email

--- app/Helpers/services.php
+++ app/Helpers/services.php
@@ -5,3 +5,10 @@ function verifyTransfer(): array
     $messages = ['Autorizado', 'Nao_Autorizado'];
     return ['message' => $messages[array_rand($messages, 1)]];
 }
+
+
+function sendMail(): array
+{
+    $messages = ['Success', 'Error'];
+    return ['message' => $messages[array_rand($messages, 1)]];
+}
--- app/Repositories/TransferRepository.php
+++ app/Repositories/TransferRepository.php
@@ -4,18 +4,22 @@
 namespace App\Repositories;
 use App\Models\Transfer;
 use App\Models\Wallet;
+use App\Models\User;
+use App\Mail\Transfer as ReceiverMail;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
 class TransferRepository implements TransferInterface
 {
 
-    private $model, $wallet;
+    private $model, $wallet, $user;
 
-    public function __construct(Transfer $model, Wallet $wallet)
+    public function __construct(Transfer $model, Wallet $wallet, User $user)
     {
         $this->model = $model;
         $this->wallet = $wallet;
+        $this->user = $user;
     }
 
     public function make(Array $data): object
@@ -28,7 +32,7 @@ class TransferRepository implements TransferInterface
             $value = (float) $data['value'];
             $balance = (float) $this->wallet->where('user_id', $user_id)->first()->value;
             $balanceReceiver = (float) $this->wallet->where('user_id', $receiver)->first()->value;
-
+            
             if($balance < $value) {
                 return response()->json(['message' => 'Saldo insuficiente'], Response::HTTP_UNPROCESSABLE_ENTITY);
             }
@@ -44,6 +48,12 @@ class TransferRepository implements TransferInterface
                     $sum = $balanceReceiver + $value;
                     $this->wallet->find($receiver)->update(['value' => $sum]);
                     DB::commit();
+
+                    $sendMail = sendMail()['message'];
+                    if($sendMail === 'Success') {
+                        $to = $this->user->find($receiver);
+                        Mail::to($to->email)->send(new ReceiverMail());
+                    }
                     return response()->json(['message' => 'Transferência realizada com sucesso'], Response::HTTP_OK);
                 }
             }
